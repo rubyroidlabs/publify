4f8ae16ab220568409e9b5955203e25accfb0da9
Yury Kaliada <fut.wrk@gmail.com>
2014-12-14 17:23:59 +0300

    add unicorn config
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

---
+++ config/unicorn.rb
@@ -0,0 +1,70 @@
+# Production specific settings
+app_dir = "publify"
+worker_processes 3
+
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
+listen "/tmp/unicorn_#{app_dir}.sock", :backlog => 64
+
+# Preload our app for more speed
+preload_app true
+
+# nuke workers after 30 seconds instead of 60 seconds (the default)
+timeout 30
+
+# Help ensure your application will always spawn in the symlinked
+# "current" directory that Capistrano sets up.
+working_directory "/home/deployer/apps/#{app_dir}/current"
+# feel free to point this anywhere accessible on the filesystem
+user 'deployer', 'deployer'
+
+shared_path = "/home/deployer/apps/#{app_dir}/shared"
+
+stderr_path "#{shared_path}/log/unicorn.stderr.log"
+stdout_path "#{shared_path}/log/unicorn.stdout.log"
+
+pid "#{shared_path}/tmp/pids/unicorn.pid"
+
+
+before_fork do |server, worker|
+  # the following is highly recommended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection_pool.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
+
+  # The following is only recommended for memory/DB-constrained
+  # installations.  It is not needed if your system can house
+  # twice as many worker_processes as you have configured.
+
+  # This allows a new master process to incrementally
+  # phase out the old master process with SIGTTOU to avoid a
+  # thundering herd (especially in the "preload_app false" case)
+  # when doing a transparent upgrade.  The last worker spawned
+  # will then kill off the old master process with a SIGQUIT.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+
+  # Throttle the master from forking too quickly by sleeping.  Due
+  # to the implementation of standard Unix signal handlers, this
+  # helps (but does not completely) prevent identical, repeated signals
+  # from being lost when the receiving process is busy.
+  # sleep 1
+end
+
+after_fork do |server, worker|
+  # per-process listener ports for debugging/admin/migrations
+  # addr = "127.0.0.1:#{9293 + worker.nr}"
+  # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
+
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
